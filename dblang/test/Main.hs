99f0cd68b923ed951e443f99e96b53caabe1c201
Isaac Elliott <isaace71295@gmail.com>
2023-04-13 17:09:06 +1000

    factor out tests into dblang-postgres

--- dblang/test/Main.hs
+++ dblang/test/Main.hs
@@ -1,6 +1,5 @@
 module Main (main) where
 
-import qualified Test.Dblang.Compile.Postgres
 import qualified Test.Dblang.Parse
 import qualified Test.Dblang.Typecheck
 import Test.Hspec (hspec)
@@ -8,6 +7,5 @@ import Test.Hspec (hspec)
 main :: IO ()
 main =
   hspec $ do
-    Test.Dblang.Compile.Postgres.spec
     Test.Dblang.Parse.spec
     Test.Dblang.Typecheck.spec
